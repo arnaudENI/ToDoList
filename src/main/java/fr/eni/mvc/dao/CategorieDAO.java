f38e2f9ba5e90640e55321dafb024b45e8ac8d3d
arnaud <arnaud.coste.dl@gmail.com>
2020-05-29 13:39:25 +0200

    methode categorie et taches

--- src/main/java/fr/eni/mvc/dao/CategorieDAO.java
+++ src/main/java/fr/eni/mvc/dao/CategorieDAO.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.eni.mvc.bean.Categorie;
+import fr.eni.mvc.bean.Tache;
 
 public interface CategorieDAO extends JpaRepository<Categorie, Integer> {
 
-	//List<Categorie> findByTaches();
+	List<Categorie> findByTaches(Tache tache);
 	
 }
